abb7db8547aff0b1e66349474b6321d13df984c4
Claire West <claireannewest@gmail.com>
2021-04-07 21:24:51 -0700

    asymptotic Green

---
+++ example_sphere/cluster_scripts/make_dirs.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+from shutil import copyfile
+
+def make_directories(intermsof, start, finish, num):
+	if intermsof == 'eV':
+		points = np.linspace(start, finish, num)
+		for i in points:
+			name = str("%.3f" % i) + str('_eV')
+			os.mkdir(name)
+			copyfile('ddscat.par', str(name)+str('/ddscat.par'))
+			new_ddscatpar = open(str(name)+str('/ddscat.par'))
+			lines = new_ddscatpar.readlines()
+			new_wavelength = np.round(1.240/i,4)
+			new_string = str(' ') + str("%.4f" % new_wavelength) + str(' ') + str("%.4f" % new_wavelength) + str(" 1 'INV' = wavelengths (first,last,how many,how=LIN,INV,LOG)" + '\n')
+			lines[26] = new_string
+			new_ddscatpar = open(str(name)+str('/ddscat.par'), "w")
+			new_ddscatpar.writelines(lines)
+			new_ddscatpar.close()
+			copyfile('shape.dat', str(name)+str('/shape.dat'))
+
+def make_submissionscripts(intermsof, start, finish, num, howmany):
+	points = np.linspace(start, finish, num)
+	folders = []
+	for i in points:
+		name = str("%.3f" % i) + str('_eV')
+		folders = np.append(folders, name)
+	num_files = int(num/howmany)+1
+	for j in range(0, num_files):
+		copyfile('launch_temp.slurm', str('launch_part')+str(j)+str('.slurm'))
+		new_launch = open(str('launch_part')+str(j)+str('.slurm'))
+		lines = new_launch.readlines()
+
+		thepoints = folders[j*howmany : (j+1)*howmany]
+		new_string = str('array=( ')+' '.join(repr(i) for i in thepoints).replace("'", '"') + str(' )') + str('\n')
+		lines[2] = str('#SBATCH --job-name=p')+str(j)+str('\n')
+		lines[22] = new_string
+		new_launch = open(str('launch_part')+str(j)+str('.slurm'),"w")
+		new_launch.writelines(lines)
+		new_launch.close()
+
+
+make_submissionscripts(intermsof='eV', start=1, finish=2.5, num=71,howmany=25)
---
+++ example_sphere/cluster_scripts/quick_remove.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -r *eV
+rm -r __pycache__
+rm *~
+rm *#*
+rm Spectrum
+rm temp.out
+rm tdda_input
